094e405d9282cf77326e102dba180377ed04552b
erwinnunez <er.nunez@duocuc.cl>
2023-09-05 09:41:04 -0300

    login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  usuario={
+    username:'',
+    password:''
+  }
+
+  constructor(
+    private router:Router,
+    private alertController:AlertController
+    ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit()
+  {
+    if (this.usuario.username=="admin@admin.cl" && this.usuario.password=="12345"){
+      this.router.navigate(['/home'])
+    }
+    else{
+      this.presentAlert()
+    }
+
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Información',
+      message: "Usuario y/o password incorrectos",
+      buttons: ['OK'],
+      backdropDismiss:false,
+      
+    });
+
+    await alert.present();
+
+ 
   }
 
 }
