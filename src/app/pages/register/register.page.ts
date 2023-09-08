ea59acc6b80f392c3d2c6d823f5ff807ddd4d9df
erwinnunez <er.nunez@duocuc.cl>
2023-09-07 22:58:48 -0300

    register

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  registro={
+    nombre:'',
+    apellido:'',
+    email:'',
+    password:'',
+    password2:''
+  }
 
-  constructor() { }
+  constructor(
+    private router:Router,
+    private alertController:AlertController
+    ) { }
 
   ngOnInit() {
   }
+
+  onSubmit()
+  {
+    this.router.navigate(['/home'])
+  }
+
+  irLogin(){
+    this.router.navigate(['/login'])
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Información',
+      message: "Algun campo se encuentra vacio",
+      buttons: ['OK'],
+      backdropDismiss:false,
+      
+    });
+
+    await alert.present();}
 
 }
